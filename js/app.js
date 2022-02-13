c622ab5b475ed9d6763483e1f4bbb3163fd3e351
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-02-13 18:20:02 +0600

    matched part complete also site is ready

--- js/app.js
+++ js/app.js
@@ -24,10 +24,31 @@ document.getElementById('key-pad').addEventListener('click', function(e) {
         if (number === 'C')
             calculateInput.value = ''
     } else if (number === '&lt;') {
-        calculateInput.value = calculateInput.value - 1
+        calculateInput.value = parseInt(calculateInput.value) - 1
     } else {
         const previousNumber = calculateInput.value
         const newNumber = previousNumber + number
         calculateInput.value = newNumber
     }
 })
+
+
+// verify pin
+function verifyPin() {
+    const viewPin = document.getElementById('view-pin').value
+    const inputNumber = document.getElementById('input-number').value
+
+    const blankSpace = document.getElementById('notify-error')
+    const pinMatched = document.getElementById('notify-success')
+    const pinError = document.getElementById('notify-error')
+
+    if (viewPin == '' && inputNumber == '') {
+        blankSpace.style.display = 'block'
+    } else if (viewPin == inputNumber) {
+        pinMatched.style.display = 'block'
+        pinError.style.display = 'none'
+    } else {
+        pinError.style.display = 'block'
+        pinMatched.style.display = 'none'
+    }
+}
